5eedb40a4dc0574764314b8d0c33d6a6634c989e
epwalsh <epwalsh@users.noreply.github.com>
2020-09-22 19:10:41 +0000

    Deploy to GitHub pages

---
+++ lib.rs
@@ -0,0 +1,12 @@
+#[macro_use]
+extern crate doc_comment;
+
+doctest!("./SUMMARY.md");
+doctest!("./what-is-celery.md");
+doctest!("./quick-start.md");
+doctest!("./guide/index.md");
+doctest!("./guide/defining-tasks.md");
+doctest!("./guide/running-workers.md");
+doctest!("./best-practices/index.md");
+doctest!("./coming-from-python/index.md");
+doctest!("./additional-resources.md");
